6f7e4d3d64ba346a0b03faaba928d4605efbf057
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-06-16 11:44:59 +0430

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-
+import Navbar from "./components/nav";
+import "./components/globalStyle.css";
 
 function App() {
   return (
-    <></>
+    <section className="hero">
+      <Navbar />
+      <section className="info">
+        <h1>
+          I'm <span>Shaden</span> Ghasri
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit,
+          <br />
+          Tempora omnis impedit necessitatibus.
+        </p>
+        <a href="#">Download CV</a>
+      </section>
+      <section class="img-box">
+        <img src="images/pattern.png" className="black-img" />
+        <img src="images/girl.png" className="main-img" />
+      </section>
+      <section class="socials">
+        <a href="">
+          <i className="fab fa-linkedin-in"></i>
+        </a>
+        <a href="">
+          <i className="fab fa-behance"></i>
+        </a>
+        <a href="">
+          <i className="fab fa-dribbble"></i>
+        </a>
+      </section>
+    </section>
   );
 }
 
---
+++ src/components/nav.js
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+import "./globalStyle.css"
+
+const Navbar = () => {
+    const handleIcon = ()=>{
+
+        const icon = document.getElementById("icon");
+        document.body.classList.toggle("dark");
+        if (document.body.classList.contains("dark")) {
+          icon.src = "sun.png";
+        } else {
+          icon.src = "moon.png";
+        }
+    }
+
+  return (
+    <>
+      <nav>
+        <img src="" alt="logo" className="logo" />
+        <ul>
+          <li>
+            <a href="#">HOME</a>
+          </li>
+          <li>
+            <a href="#">ABOUT</a>
+          </li>
+          <li>
+            <a href="#">PORTFOLIO</a>
+          </li>
+          <li>
+            <a href="#">SERVICES</a>
+          </li>
+          <li>
+            <a href="#">HIRE ME</a>
+          </li>
+        </ul>
+        <img src="sun.png" id="icon" onClick={handleIcon}/>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
+<>
+  <nav>
+    <img src="images/logo.png" alt="logo" class="logo" />
+    <ul>
+      <li>
+        <a href="#">HOME</a>
+      </li>
+      <li>
+        <a href="#">ABOUT</a>
+      </li>
+      <li>
+        <a href="#">PORTFOLIO</a>
+      </li>
+      <li>
+        <a href="#">SERVICES</a>
+      </li>
+      <li>
+        <a href="#">HIRE ME</a>
+      </li>
+    </ul>
+    <img src="icons/moon.png" id="icon" />
+  </nav>
+</>;
