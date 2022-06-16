929d57b9fac23287ee0e71eb1ca38d811c8e57ee
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-06-16 13:05:21 +0430

    commit

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,36 +1,55 @@
 import { Link } from "react-router-dom";
-import "./globalStyle.css"
-import {Collapse} from 'react-collapse';
+import "./globalStyle.css";
+import { Collapse } from "react-collapse";
 
 const Navbar = () => {
-    const handleIcon = ()=>{
-
-        const icon = document.getElementById("icon");
-        document.body.classList.toggle("dark");
-        if (document.body.classList.contains("dark")) {
-          icon.src = "sun.png";
-        } else {
-          icon.src = "moon.png";
-        }
+  const handleIcon = () => {
+    const icon = document.getElementById("icon");
+    document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
+      icon.src = "sun.png";
+    } else {
+      icon.src = "moon.png";
     }
+  };
 
   return (
     <>
       <nav className="navbar">
         <img src="hamresaan.png" alt="logo" className="logo" />
         <ul>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            HOME
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              HOME
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <hr class="dropdown-divider" />
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </li>
           <li>
             <a href="#">ABOUT</a>
           </li>
@@ -44,7 +63,7 @@ const Navbar = () => {
             <a href="#">HIRE ME</a>
           </li>
         </ul>
-        <img src="moon.png" id="icon" onClick={handleIcon}/>
+        <img src="moon.png" id="icon" onClick={handleIcon} />
       </nav>
     </>
   );
